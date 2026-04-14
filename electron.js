1866f72292028cb8c65a997194f1e18e48f4df01
iron127-iron <iron127127@gmail.com>
2026-04-15 01:22:37 +0800

    Refactor window creation in electron.js

--- electron.js
+++ electron.js
@@ -1,10 +1,12 @@
 const { app, BrowserWindow } = require("electron");
 
-app.whenReady().then(() => {
+function createWindow() {
   const win = new BrowserWindow({
     width: 1200,
     height: 800
   });
 
   win.loadURL("http://localhost:5173");
-});
+}
+
+app.whenReady().then(createWindow);
